919fcbcb836679e933c67cc005b0d3b3c22ac6fb
Aayush Soni <soniaayush24400@gmail.com>
2025-11-07 20:50:47 +0530

    added project-aws-iam-yaml

---
+++ project-aws-iam-yaml/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.19.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1" 
+}
+
+locals{
+    users_data = yamldecode(file("./users.yaml")).users
+
+    user_role_pair = flatten([for user in local.users_data:[for role in user.roles: {
+        username = user.username
+        role = role
+        } ] ])
+}
+
+output "data" {
+  value = local.user_role_pair
+}
+
+#creating users
+
+resource "aws_iam_user" "users" {
+  for_each = toset(local.users_data[*].username) #foreach only works with sets and maps hence converting to sets
+  name = each.value
+}
+
+# Password creation
+
+resource "aws_iam_user_login_profile" "password" {
+  for_each = aws_iam_user.users
+  user = each.value.name
+  password_length = 12
+    lifecycle {
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key,
+    ]
+  }
+}
+
+#attaching policies
+
+resource "aws_iam_user_policy_attachment" "main" {
+  for_each = {
+    for pair in local.user_role_pair : 
+    "${pair.username}-${pair.role}" => pair
+  }
+  user = aws_iam_user.users[each.value.username].name
+  policy_arn = "arn:aws:iam::aws:policy/${each.value.role}"
+}
